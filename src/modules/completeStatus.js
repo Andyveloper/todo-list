c05c09d56b4d9cc95c33aaeb434b21e45efe7095
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-06-04 13:57:14 -0500

    Remove logs/ Finish localStorage

--- src/modules/completeStatus.js
+++ src/modules/completeStatus.js
@@ -1,10 +1,8 @@
 // import { toDo } from '../index.js';
-import { toDo } from '../index.js';
 import { setData } from './localStorage.js';
 import { overwriteIndex, overWriteLiId } from './newTask.js';
 
 export const markAsCompleted = (todo, listItem) => {
-  console.log('hello world');
   listItem.classList.toggle('done');
   for (let i = 0; i < todo.length; i++) {
     if (todo[i].index === Number(listItem.id)) {
@@ -16,11 +14,10 @@ export const markAsCompleted = (todo, listItem) => {
     setData();
   }
 };
-
+// finally got it to work
 export const completed = (todo) => {
   const completedTasks = todo.filter((task) => task.completed === false);
   overwriteIndex(completedTasks);
   overWriteLiId();
   localStorage.setItem('toDo', JSON.stringify(completedTasks));
-  console.log(toDo);
 };
